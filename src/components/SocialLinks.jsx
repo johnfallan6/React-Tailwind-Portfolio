0123491d5129dc2ccc2950fd6e232c49e5d5b9b8
John Allan <johnfallan6@gmail.com>
2022-09-26 09:38:04 -0500

    Resume Update

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -40,7 +40,7 @@ const SocialLinks = () => {
           Resume <BsFillPersonLinesFill size={30} className="icon" />
         </>
       ),
-      href: "/John Allan_Resume 2022.pdf",
+      href: "/John Allan_Resume 2022 - September.pdf",
       style: "rounded-br-md",
       download: true,
     },
