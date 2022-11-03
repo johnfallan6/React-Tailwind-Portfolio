0a618a9bf4b55ccd79408315b3b18a979c710a55
John Allan <johnfallan6@gmail.com>
2022-11-03 12:27:02 -0500

    blog link added

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -4,21 +4,26 @@ const Portfolio = () => {
   const portfolios = [
     {
       id: 1,
-      src: "/images/btxLogo.png",
+      src: "/images/blog.png",
+      alt: "john allan blog",
+      name: "John Allan - Blog",
+      href: "https://blog-seven-flame.vercel.app/",
     },
     {
       id: 2,
       src: "/images/btxLogo.png",
+      alt: "backyard texas logo",
     },
     {
       id: 3,
       src: "/images/btxLogo.png",
+      alt: "backyard texas logo",
     },
   ];
 
   return (
     <div
-    id="portfolio"
+      id="portfolio"
       name="portfolio"
       className="bg-gradient-to-b from-darkBackground to-black text-paragraph w-full md:h-screen "
     >
@@ -28,23 +33,22 @@ const Portfolio = () => {
             Portfolio
           </h2>
           <h3 className="py-8 mt-6 text-2xl tracking-widest">
-            Check out some of my work right here! COMING SOON!!
+            Check out some of my work right here!
           </h3>
         </div>
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {portfolios.map(({ id, src }) => (
+          {portfolios.map(({ id, src, alt, href, name }) => (
             <div key={id} className="shadow-md shadow-secondary rounded-lg">
               <img
                 src={src}
-                alt="Logo for BTX custom boards"
+                alt={alt}
                 className="rounded-md duration-200 hover:scale-105"
               />
               <div className="flex items-center justify-center text-header">
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                  Demo
-                </button>
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                  Code
+                <button className="w-1/2 px-6 py-3 m-4 duration-300 hover:scale-110 hover:text-primary">
+                  <a href={href} target="_blank" rel="noreferrer">
+                    {name}
+                  </a>
                 </button>
               </div>
             </div>
